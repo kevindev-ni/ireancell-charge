f752f4f0fe1c27a0f1508f5df05b9c0d5b8bf1e7
rezakarimi <rezakarimi@haiocloud.com>
2022-08-06 15:04:51 +0430

    update and handle change locale (HandleLocale)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import type { NextPage } from 'next';
-import {useSelector} from "react-redux";
-import {selectCounterState} from "../src/redux/store/counterSlice";
 import AppContainer from "../src/layouts/AppContainer";
 import {useRouter} from "next/router";
 import useTranslation from "next-translate/useTranslation";
 
 const Home: NextPage = () => {
-    const a = useSelector(selectCounterState)
     const {t} = useTranslation('common')
     const router = useRouter()
     console.log('lang local', router.locale)
-    console.log('a', a)
   return (
       <AppContainer>
             <div className={'flex justify-center'}>
--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -3,10 +3,18 @@ import {AppBar, Box, Toolbar, Button, Typography, IconButton} from "@mui/materia
 import LanguageIconIcon from '@mui/icons-material/Language';
 import {useDispatch, useSelector} from "react-redux";
 import {changeLang, selectLocalState} from "../../redux/store/langSlice";
+import {useRouter} from "next/router";
+import Link from "next/link";
 
 const Header = () => {
     const dispatch = useDispatch()
+    const router = useRouter()
     const {lang} = useSelector(selectLocalState)
+    const handleLocate = router.locale === 'EN' ? 'FA' : 'EN'
+    const HandleLocale = () => {
+        dispatch(changeLang(handleLocate))
+        router.push('/', '/', {locale: handleLocate})
+    }
     return (
         <AppBar color={"primary"} component={"nav"}>
             <Toolbar className={'p-0 block'}>
@@ -14,7 +22,8 @@ const Header = () => {
                     <Typography className={'vazir-med'}>
                         ایرانسل
                     </Typography>
-                    <Box onClick={() => dispatch(changeLang('EN'))} className={'bg-black h-[80px] flex items-center justify-center p-c0 m-0 w-[200px]'}>
+                    <Box onClick={HandleLocale}
+                         className={'bg-black h-[80px] flex items-center justify-center p-c0 m-0 w-[200px]'}>
                         <Button startIcon={<LanguageIconIcon color={'primary'}/>} size={'small'} className={'text-sm'}>
                             {lang}
                         </Button>
