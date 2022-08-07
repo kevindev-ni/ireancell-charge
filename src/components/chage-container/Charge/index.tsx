8c64394288bbe645caad2443ba50f772432e0a65
rezakarimi <rezakarimi@haiocloud.com>
2022-08-07 10:00:39 +0430

    hanle select simcard type

--- src/components/chage-container/Charge/index.tsx
+++ src/components/chage-container/Charge/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Box, Typography} from "@mui/material";
 
 const Charge = () => {
     return (
-        <div>
-            
-        </div>
+        <Box className={'flex flex-col items-center justify-center'}>
+            <Typography>خرید آنلاین شارژ ایرانسل </Typography>
+            <Box
+                className={'flex justify-between items-center w-4/12 mt-5 border-[1px] rounded-[25px] border-[#e6e6e8]'}>
+                <div className={'text-center w-full px-[7px] py-[7px] '}>
+                    <button>
+                        اعتباری
+                    </button>
+                </div>
+                <div className={'text-center rounded-[25px] bg-yellow-500 p-1 px-[7px] py-[7px]  w-full'}>
+                    <button>
+                        دائمی
+                    </button>
+                </div>
+            </Box>
+        </Box>
     );
 };
 
